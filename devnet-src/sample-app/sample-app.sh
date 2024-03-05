c33c677f4d1ab4840c59194a3895fe2811feb60d
wyonghao <wyonghao@outlook.com>
2024-03-05 14:10:24 +0000

    Changed port from 8080 to 5050.

--- devnet-src/sample-app/sample-app.sh
+++ devnet-src/sample-app/sample-app.sh
@@ -1,9 +1,25 @@
-from flask import Flask
-from flask import request
-sample = Flask(__name__)
-@sample.route("/")
-def main()
-    return "You are calling me from " + request.remote_addr + "/n"
-if __name__=="__main__":
-    sample.run(host="0.0.0.0", port=8080)
+#!/bin/bash
+
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+
+docker build -t sampleapp .
+
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a
 
